7eee5fd9dde76b81ecbca2b13b275f60035c3009
aSwin <aSwin@gmail.com>
2026-03-02 17:24:45 +0530

    bootstrapped customer module

--- core/forms.py
+++ core/forms.py
@@ -18,6 +18,14 @@ class BookingForm(forms.ModelForm):
             'event_date': forms.DateInput(attrs={'type': 'date'})
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'form-control form-control-lg'
+            })
+
 class CueForm(forms.ModelForm):
 
     class Meta:
