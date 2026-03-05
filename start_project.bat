8c9d4d5940a2153cfc96b573eceab3f58af56268
aSwin <aSwin@gmail.com>
2026-03-05 18:46:26 +0530

    deleted

--- start_project.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d %~dp0
-
-start cmd /k "C:\Redis\redis-server.exe"
-start cmd /k "venv\Scripts\activate && python manage.py runserver"
-start cmd /k "venv\Scripts\activate && python manage.py run_cue_engine"
